0d1d93f4b5cc3abef78dea91e1a089c5557bf713
mohamedsaied8 <infint_hero@hotmail.com>
2019-12-20 13:45:33 +0200

    circuits

--- Projects/LM35_Sensor/Debug/lcd.d
+++ Projects/LM35_Sensor/Debug/lcd.d
@@ -4,7 +4,7 @@ lcd.d lcd.o: .././lcd.c .././lcd.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom32.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
@@ -26,7 +26,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom32.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom32.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
--- Projects/LM35_Sensor/main.c
+++ Projects/LM35_Sensor/main.c
@@ -11,21 +11,27 @@
 #include "lcd.h"
 
 #define degree_sysmbol 0xdf
+#define ADMUX0		0
 
 void ADC_Init(){										
 	DDRA = 0xF0;							        /* Make ADC port as input */
-	ADCSRA = 0x87;									/* Enable ADC, with freq/128  */
-	ADMUX = 0x40;									/* Vref: Avcc, ADC channel: 0 */
+	ADCSRA =(1<<ADPS0) | (1<<ADPS1)	|(1<<ADPS2) | (1<<ADATE) | (1<<ADEN);							/* Enable ADC, with freq/128  */
+	ADMUX =(1<<REFS0) | (1<<REFS1) |(1<<ADMUX0);		/* Vref: Internal , ADC channel: 1 */
 }
 
 int ADC_Read(char channel)							
-{
-	ADMUX = 0x40 | (channel & 0x07);				/* set input channel to read */
+{					 
+	ADMUX = 0xC0 | (channel & 0x07);				/* set input channel to read */
 	ADCSRA |= (1<<ADSC);							/* Start ADC conversion */
 	while (!(ADCSRA & (1<<ADIF)));					/* Wait until end of conversion by polling ADC interrupt flag */
 	ADCSRA |= (1<<ADIF);							/* Clear interrupt flag */
 	_delay_ms(1);									/* Wait a little bit */
-	return ADCW;									/* Return ADC word */
+	
+	int dataADC=ADCL;
+	dataADC=ADCH<<8;
+	return dataADC;
+	//return ADCW;									/* Return ADC word */
+	
 }
 
 
@@ -47,7 +53,7 @@ int main()
 		//LCD_String_xy(0,0,"Temperature");
 		 LCD_String("Temperature");
 		celsius = (ADC_Read(1)*4.88);
-		celsius = (celsius/10.00);
+		celsius = (celsius/10.00); //equation of temperature sensor
 		sprintf(Temperature,"%d%cC  ", (int)celsius, degree_sysmbol);               // convert integer value to ASCII string 
 		LCD_Command(0xC0);	
 		LCD_String(Temperature);                // send string data for printing  
